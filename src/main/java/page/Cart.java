cc59a10e5d81c9585f15c16e1d7dcdd3a89b76b1
amitkumardianapps <140390725+amitkumardianapps@users.noreply.github.com>
2023-11-02 10:03:14 +0530

    Added the click for the cart-page

---
+++ src/main/java/page/Cart.java
@@ -0,0 +1,12 @@
+package page;
+
+import org.openqa.selenium.By;
+
+
+public class Cart {
+
+    public static final By cartButtonLocator=By.xpath("//div[@id='shopping_cart_container']//a[contains(@class, 'shopping_cart_link')]");
+
+
+
+}
